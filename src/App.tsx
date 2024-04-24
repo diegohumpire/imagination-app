a9c8338364324515d67fdb6e7a851a00bcdad738
Diego Humpire <me@diegohumpire.me>
2024-04-24 18:45:20 -0500

    Improve UI and details

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Prompts from "./pages/Prompts.tsx";
 import ResultPrompt from "./pages/ResultPrompt.tsx";
 import Layout from "./components/Layout.tsx";
 import Page404 from "./pages/errors/Page404.tsx";
-import LoadingOverlay from "./components/LoadingOverlay.tsx";
 
 let router = createBrowserRouter([
   {
@@ -17,8 +16,7 @@ let router = createBrowserRouter([
       { path: "/prompts", element: <Prompts /> },
       { path: "/result", element: <ResultPrompt /> },
       { path: "/result-prompt", element: <ResultPrompt /> },
-      //   { path: "*", element: <Page404 /> },
-      { path: "*", element: <LoadingOverlay /> },
+      { path: "*", element: <Page404 /> },
     ],
   },
 ]);
--- src/pages/errors/Page404.tsx
+++ src/pages/errors/Page404.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Page404() {
   console.error("404 - Page Not Found");
   return (
-    <div className="page-404 text-black">
+    <div className="page-404 text-black flex h-screen flex-col justify-center items-center">
       <h1 className="title">404 - Page Not Found</h1>
       <p className="not-found-message">The page you are looking for does not exist</p>
       <Link to="/" className={"back-to-home"}>
